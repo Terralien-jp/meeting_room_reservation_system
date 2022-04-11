bea804698f9d1a2548cc515cd292ea616daedf89
Terralien-jp <terralienjp@gmail.com>
2022-04-11 20:44:53 +0900

    fix main.py

--- sql_app/main.py
+++ sql_app/main.py
@@ -37,14 +37,14 @@ async def read_bookings(skip: int = 0, limit: int = 100, db: Session = Depends(g
     return bookings
 
 # Create
-@app.post("/users")
-async def users(users: User):
-    return {"users": users}
+@app.post("/users",  response_mnodel=schemas.User)
+async def create_users(user: schemas.User, db: Session =Depends(get_db)):
+    return crud.create_user(de=db, user=user)
 
-@app.post("/rooms")
-async def rooms(rooms: Room):
-    return {"rooms": rooms}
+@app.post("/rooms",  response_mnodel=schemas.Room)
+async def create_rooms(room: schemas.Room, db: Session =Depends(get_db)):
+    return crud.create_room(de=db, room=room)
 
-@app.post("/bookings")
-async def bookings(bookings: Booking):
-    return {"bookings": bookings}
+@app.post("/bookings",  response_mnodel=schemas.Booking)
+async def create_bookings(booking: schemas.Booking, db: Session =Depends(get_db)):
+    return crud.create_booking(de=db, booking=booking)
