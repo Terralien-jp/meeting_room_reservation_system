77c1397527f2d37dbb0cf453701380a82a7dabb7
Terralien-jp <terralienjp@gmail.com>
2022-04-15 20:07:02 +0900

    fix crud.py

--- app.py
+++ app.py
@@ -154,8 +154,15 @@ elif page == 'bookings':
                 minute=end_time.minute
             ).isoformat()
         }
-        # 定員以下の予約人数の場合
-        if booked_num <= capacity:
+        # 定員より多い予約人数の場合
+        if booked_num > capacity:
+            st.error(f'{room_name}の定員は、{capacity}名です。{capacity}名以下の予約のみ受け付けております。')
+        # 開始時刻が終了時刻より大きい等
+        elif start_time >= end_time:
+            st.error('開始時刻が終了時刻を超えています')
+        elif start_time < datetime.time(hour=9, minute=0, second=0) or end_time > datetime.time(hour=20, minute=0, second=0):
+            st.error('利用時間は9:00~20:00になります')
+        else:
             # 会議室予約
             url = 'http://127.0.0.1:8000/bookings'
             res = requests.post(
@@ -165,5 +172,3 @@ elif page == 'bookings':
             if res.status_code == 200:
                 st.success('予約完了しました')
             st.json(res.json())
-        else:
-            st.error(f'{room_name}の定員は、{capacity}名です。{capacity}名以下の予約のみ受け付けております。')
